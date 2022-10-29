9233ea76511e0c8915ce1c74f8520188ad10dabe
FerArj <fernando.araujo@sptech.school>
2022-10-29 00:12:37 -0300

    Textos e func

---
+++ WebSite/js/app.js
@@ -0,0 +1,24 @@
+function ler_mais_conteudo1(){
+    descricao1.style.display = "block";
+}
+
+function fechar_descricao1(){
+    descricao1.style.display = "none";
+}
+
+function ler_mais_conteudo2(){
+    descricao2.style.display = "block";
+}
+
+function fechar_descricao2(){
+    descricao2.style.display = "none";
+}
+
+function ler_mais_conteudo3(){
+    descricao3.style.display = "block";
+}
+
+function fechar_descricao3(){
+    descricao3.style.display = "none";
+}
+
