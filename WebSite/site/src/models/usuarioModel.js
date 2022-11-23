9e0e6ce774043b250aad40727b6b823738c0a270
FerArj <fernando.araujo@sptech.school>
2022-11-23 13:22:45 -0300

    validação finalizada

--- WebSite/site/src/models/usuarioModel.js
+++ WebSite/site/src/models/usuarioModel.js
@@ -12,7 +12,7 @@ function listar() {
 function entrar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucao = `
-        SELECT * FROM usuario WHERE email = '${email}' AND senha = '${senha}';
+        SELECT * FROM usuario WHERE email = '${email}' AND senha = sha2('${senha}', 256);
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
@@ -25,7 +25,7 @@ function cadastrar(email, usuario, senha, fkVoto, voto) {
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucao = `
-        INSERT INTO usuario (email, nome_usuario, senha, fkVotacao) VALUES ('${email}', '${usuario}', '${senha}', '${fkVoto}');
+        INSERT INTO usuario (email, nome_usuario, senha, fkVotacao) VALUES ('${email}', '${usuario}', sha2('${senha}', 256), '${fkVoto}');
         `;
 
     console.log("Executando a instrução SQL: \n" + instrucao);
